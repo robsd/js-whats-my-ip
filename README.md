4473c58832dd5af3117409a04b0222412f616fa7
Robert Dixon <79385125+robsd@users.noreply.github.com>
2021-10-21 01:00:07 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# js-whats-my-ip 🌐
+
+A JavaScript application that implements the IP Info API to look up your IP address and return a few other details including hostname, ISP and location info.
+
+## Demo
+
+https://robsd.github.io/js-whats-my-ip
---
+++ js/script.js
@@ -0,0 +1,35 @@
+main = document.getElementById('main');
+error = document.getElementById('error');
+
+ip = document.getElementById('ip');
+hostname = document.getElementById('hostname');
+isp = document.getElementById('isp');
+city = document.getElementById('city');
+region = document.getElementById('region');
+country = document.getElementById('country');
+
+request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+	if (this.readyState == 4) {
+		if (this.status == 200) {
+			response = JSON.parse(this.responseText);
+
+			ip.innerHTML = response['ip'];
+			hostname.innerHTML = response['hostname'];
+			isp.innerHTML = response['org'];
+			city.innerHTML = response['city'];
+			region.innerHTML = response['region'];
+			country.innerHTML = response['country'];
+
+			error.classList.value = 'd-none';
+			main.classList.value = 'text-center';
+		}
+		else {
+			main.classList.value = 'd-none';
+			error.classList.value = 'alert alert-danger';
+			error.innerHTML = 'Something went wrong.';
+		}
+	}
+};
+request.open('GET', 'https://ipinfo.io/json');
+request.send();
